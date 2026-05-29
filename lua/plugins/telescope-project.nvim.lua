ec42e04675c2a6ea33e230c9f3ee1fdb8b7608e7
Ablasse Kingcaid-Ouedraogo <ablassecode@outlook.com>
2026-05-29 13:45:12 -0500

    refactor(projects): drop telescope-project in favor of project.nvim
    project.nvim's telescope extension (added in the prior commit) replaces
    the standalone telescope-project plugin.

--- lua/plugins/telescope-project.nvim.lua
+++
@@ -1,9 +0,0 @@
--- Plugin: nvim-telescope/telescope-project.nvim
--- Installed via store.nvim
-
-return {
-    "nvim-telescope/telescope-project.nvim",
-    dependencies = {
-        "nvim-telescope/telescope.nvim"
-    }
-}
